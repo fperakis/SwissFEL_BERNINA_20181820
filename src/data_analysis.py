f583cf3efcbe225573a4aa5978a11aa724414fc3
fperakis <f.perakis@fysik.su.se>
2019-01-24 16:57:09 +0100

    updated the data analysis scripts

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -21,7 +21,7 @@ def process_run(run):
     data = swissfel.parseScanEco_v01(path,createEscArrays=True,memlimit_mD_MB=50)
     jf7 = data['JF07T32V01'] # JungFrau data
     num_shots = jf7.data.shape[jf7.eventDim]
-    
+ 
     # load corrections
     pede,noise,mask = load_corrections()
 
@@ -33,8 +33,10 @@ def process_run(run):
         icorr = apply_gain_pede(jf7.data[i_shot].compute(),G=gains, P=pede, pixel_mask=mask)
         icorr_geom += apply_geometry(icorr,'JF07T32V01')
         print('%.1f Hz'%(1.0/(time.time() - t1)))
-    
-    save_data = np.array([])
+
+    save_data = np.array([icorr_geom, num_shots])
+    save_path = '/sf/bernina/data/p17743/res/scratch/hdf5/run%.h5'
+    save_h5(save_path,save_data)
     return icorr_geom,num_shots
 
 
@@ -52,15 +54,15 @@ with h5py.File('/sf/bernina/data/p17743/res/JF_pedestals/pedestal_20190115_1551.
     return pede,noise,mask
 
 
-def save_data_h5(filename,save_data):
+def save_h5(save_path,save_data):
     '''
     Saves the processed data in h5.
     '''
 
     avg_img_2d,num_shots = save_data
 
-    h5f = h5py.File(filename, 'w')
-    
+    h5f = h5py.File(save_path, 'w')
+
     IMG_2D = h5f.create_group("Average_2D_Image")
     IMG_2D.create_dataset("2D_img", data = avg_img_2d, dtype = i)
     IMG_2D.create_dataset("num_shots", data = num_shots, dtype = i)
