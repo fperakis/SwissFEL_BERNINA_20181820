c593feabfac42ff28987445ce69cf9e2b268e245
Sellberg Jonas <ext-sellberg_j@ra-l-002.psi.ch>
2019-01-29 23:01:07 +0100

    changed output hdf5 folder to /res/work/hdf5

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -56,7 +56,7 @@ def process_run(run,path,num_shots=0):
         print('run%s - s.%i - %.1f Hz'%(run,i_shot,1.0/(time.time() - t1)))
 
     save_data = np.array([icorr_sum, num_shots,r,iqs])
-    save_path = '/sf/bernina/data/p17743/scratch/hdf5/run%s.h5'%run
+    save_path = '/sf/bernina/data/p17743/res/work/hdf5/run%s.h5'%run
     print('-- Saving data: %s'%save_path)
     save_h5(save_path,save_data)
     return
