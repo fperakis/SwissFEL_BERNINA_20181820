b78a8b115021dcbc9819d1864d5377df5f6ee12a
fperakis <f.perakis@fysik.su.se>
2019-01-30 21:54:55 +0100

    working on the analysis

--- scripts/job.sh
+++ scripts/job.sh
@@ -3,8 +3,12 @@
 #SBATCH --job-name=process
 #SBATCH --output=job_output.txt
 
-srun  ./process.py -r 0018_droplets_10um_2mm -s 1000
-srun  ./process.py -r 0019_droplets_10um_2mm -s 1000
-srun  ./process.py -r 0020_droplets_10um_2mm -s 1000
-srun  ./process.py -r 0021_droplets_10um_2mm -s 1000
-srun  ./process.py -r 0022_droplets_10um_2mm -s 1000
+srun  ./process.py -r 0024_droplets_10um_4mm &
+srun  ./process.py -r 0025_droplets_10um_4mm &
+srun  ./process.py -r 0026_droplets_10um_4mm &
+srun  ./process.py -r 0027_droplets_10um_4mm &
+srun  ./process.py -r 0028_droplets_10um_4mm &
+srun  ./process.py -r 0029_droplets_10um_4mm &
+srun  ./process.py -r 0030_droplets_10um_4mm &
+srun  ./process.py -r 0031_droplets_10um_4mm &
+srun  ./process.py -r 0032_droplets_10um_4mm 
